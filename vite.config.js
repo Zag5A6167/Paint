7bf575f55651f8e6dacf78af4e2e0b40dbccf1b7
zag <za12ew44zz_999@hotmail.com>
2025-03-02 13:02:18 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/Paint/' : '/',
+    "homepage": "https://Zag5A6167.github.io/Paint",
+    "scripts": {
+      "dev": "vite",
+      "build": "vite build",
+      "serve": "vite preview",
+      "predeploy": "npm run build",
+      "deploy": "gh-pages -d dist"
+    },
+    "dependencies": {
+      "react": "^17.0.2",
+      "react-dom": "^17.0.2"
+    },
+    "devDependencies": {
+      "vite": "^2.6.4",
+      "gh-pages": "^3.2.3"
+    }
+  };
+});
